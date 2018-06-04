edb2b8d9a9ed531f8fca77b7e3a9b08cbfc6945c
Denys Sobchuk <denys.sobchuk@globallogic.com>
2018-06-04 19:38:02 +0300

    init commit

---
+++ ig_house_src/main/server.h
@@ -0,0 +1,22 @@
+#ifndef IG_SERVER
+#define IG_SERVER
+
+/* Stack
+ *  Functional objects
+ *      diffrent usr/pass
+ *      methods
+ *      properties
+ *      signals
+ *  Function object
+ *      facade of all objects
+ *      name/version/location/device model/etc
+ *  Protocol server
+ *  LwIP
+ */
+
+
+
+
+
+
+#endif
