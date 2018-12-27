c7f235c2888062fcc6d146515b712239abcefc62
AvatarSD <sd1995@ukr.net>
2018-12-27 02:23:05 +0200

    Add ToDo to NewYear 2019

--- ig_house_src/main/server.cpp
+++ ig_house_src/main/server.cpp
@@ -17,6 +17,90 @@
 #include "cJSON.h"
 
 
+
+/* TODO
+1. Construct API:
+	* Server(virt, impl logic) <- (derived, impl phy): tcp, udp, can bus, LoRa 
+		> threads:
+		accept conn, receive data to queue
+		transmit data to queue, close connection
+		close sock by  keep-alive rule
+		____
+		> data:
+		list of open connections
+			descr
+			last acces time
+			keep-alive rule: zero, some time, unlim
+			possible: statistic(heap)
+		rx/tx queue
+		in derived: addr, port, etc..
+		____
+		> logic:
+		api for close conn from timeout list
+	____
+	* ModuleRouter
+		> data:
+		list of module's
+		system module
+		 * system log
+		 * settings
+		 * os system data
+		 * error notifier
+		queues acces from modules
+		rx/tx queue pointers
+		____
+		> logic:
+		json parsing
+		client/server message routing
+		furure: auth
+	____
+	* Module(virt, impl routines) <- (derived, impl module logic)
+		> logic
+		json processing
+		notifying
+		____
+		> data
+		Notifier's list
+		Calc
+		____
+		> derived example:
+		hw: button, switch, step motor, led, lcd, etc...
+		sw: from timer tooooo.. pyton module =)
+	____
+	* Client:
+		> threads:
+		opening conn
+		____
+		> logic
+		response waiting
+		retrying
+		____
+		> data
+		list of open connections
+			descr
+			last acces time
+			keep-alive rule: zero, some time, unlim
+			possible: statistic(heap)
+		rx/tx queue
+		in derived: addr, port, etc..
+	____
+	* Calc
+		> logic:
+		subscribtions: request notifing
+		recalc on new value
+		____
+		> data
+		list of all last values
+
+
+
+
+
+
+*/
+
+
+
 JsonServer::JsonServer(bool is_bcast, QueueHandle_t out_queue)
 {
 
